2580cf894256419fff1776bf3704ca890e670611
gabrielhcsilva <39423330+gabrielhcsilva@users.noreply.github.com>
2019-07-07 15:21:25 -0300

    Update restricao1.sql

--- restricao1.sql
+++ restricao1.sql
@@ -5,27 +5,59 @@ CREATE OR REPLACE FUNCTION verifica_bolsa()
 RETURNS trigger AS $$
 DECLARE
 	minimo float;
-	
+	data_cancel date;
+	media float;
+	vs float;
+	insc_aluno int;
 BEGIN
-	-- Extrai dia do mes atual
-	SELECT DATE_PART('DAY', current_timestamp) AS dia;
-
-	-- Desempenho minimo requerido
-	SELECT b.media_minima
-	INTO minimo
-	FROM Bolsa b
-
-	-- Mudancao de mes:
-	IF (dia = 1) THEN
-		UPDATE Bolsa B
-    SET valor_porcentagem = 0
-    WHERE B.bol_aluno_id IN (SELECT A1.alu_id
-			     FROM Aluno A1
-			     INNER JOIN Inscricao I1 ON (I1.insc_aluno_id = A1.alu_id AND I1.insc_ativa)
-			     WHERE minimo > (SELECT AVG(N2.nota)
-					     FROM Nota N2
-					     INNER JOIN Inscricao I2 ON (I2.insc_id = N2.inscricao_id AND I2.insc_ativa)
-					     INNER JOIN Aluno A2 ON (I2.insc_aluno_id = A2.alu_id)));
+	-- Armazena data de cancelamento e media minima da bolsa do aluno cujas notas estao sendo utilizadas em alguma operacao
+	SELECT B.bol_data_fim, B.bol_media_min
+	INTO data_cancel, minimo
+	FROM Bolsa B
+	INNER JOIN Aluno A ON (B.bol_aluno_id = A.alu_id)
+	INNER JOIN Inscricao I ON (I.insc_aluno_id = A.alu_id)
+	WHERE NEW.inscricao_id = I.insc_id;
+	
+	-- Calcula media do aluno
+	SELECT AVG(N.nota), I1.insc_aluno_id
+	INTO media, insc_aluno
+	FROM Nota N
+	INNER JOIN Inscricao I1 ON (I1.insc_id = NEW.inscricao_id)
+	AND N.avaliacao_id IN (SELECT avaliacao_id
+			       FROM Avaliacao 
+			       WHERE nome<>'VS');
+	
+	-- Se ficou de VS, verifica nota da VS
+	IF(media < 6 AND media > 4) THEN 
+		SELECT N1.nota, I2.insc_aluno_id
+		INTO vs, insc_aluno
+		FROM Nota N1
+		INNER JOIN Inscricao I2 ON (I2.insc_id = NEW.inscricao_id)
+		AND N1.avaliacao_id IN (SELECT av_id
+					FROM Avaliacao 
+					WHERE nome='VS');
+		-- Se fez VS, altera media
+		IF (vs >= 6) THEN
+			media = 6;
+		ELSE
+			media = vs;
+		END IF;
+	END IF;
+	
+	-- Se bolsa ativa e aluno teve um mau desempenho
+	IF (data_cancel is NULL AND minimo > media) THEN
+		-- Cancela bolsa
+		UPDATE Bolsa B1
+   		SET B1.bol_data_fim = now()
+		WHERE B1.bol_aluno_id IN (SELECT A1.alu_id
+			     		 FROM Aluno A1
+			                 WHERE insc_aluno = A1.alu_id);
+		-- Altera valor das vendas do contrato
+		UPDATE Venda V1
+		SET 
+		WHERE V1.ven_contrato_id IN (SELECT V2.ven_contrato_id
+					     FROM Venda V2
+					     INNER JOIN Contrato C ON (C.contr_id = V2.ven_contrato_id AND C.contr_aluno_id = insc_aluno);
 
 	END IF;
 
